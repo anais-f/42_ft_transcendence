318617f7244a88d9bc6924cbf66340f85307120f
anfichet <anfichet@student.42lyon.fr>
2025-07-16 09:56:47 +0200

    feat: new hook for branch naming

--- hooks/install-hooks.sh
+++ hooks/install-hooks.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 cp "$(dirname "$0")/commit-msg" .git/hooks/
+cp "$(dirname "$0")/pre-push" .git/hooks/
+
 chmod +x .git/hooks/commit-msg
+chmod +x .git/hooks/pre-push
+
 echo "Hook installed !"
