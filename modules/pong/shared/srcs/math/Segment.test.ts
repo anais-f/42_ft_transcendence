7b2c5d0866da064022c47f8b9b6550fabaede0aa
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-08-05 07:49:18 +0200

    test: add polygon tests and improve Segment tests

--- modules/pong/shared/srcs/math/Segment.test.ts
+++ modules/pong/shared/srcs/math/Segment.test.ts
@@ -10,35 +10,99 @@ describe("Segment", () => {
     expect(s.getPoints()).toEqual([a, b])
   })
 
-  test("intersect with Circle - intersects", () => {
-    const a = new Vector2(0, 0)
-    const b = new Vector2(10, 0)
-    const s = new Segment(a, b)
-    const c = new Circle(new Vector2(5, 0), 2)
-    expect(s.intersect(c)).toBe(true)
-  })
+  describe("intersect", () => {
 
-  test("intersect with Circle - does not intersect", () => {
-    const a = new Vector2(0, 0)
-    const b = new Vector2(10, 0)
-    const s = new Segment(a, b)
-    const c = new Circle(new Vector2(5, 5), 2)
-    expect(s.intersect(c)).toBe(false)
-  })
+    describe("circle", () => {
+      test("basic intersect test", () => {
+        const a = new Vector2(0, 0)
+        const b = new Vector2(10, 0)
+        const s = new Segment(a, b)
+        const c = new Circle(new Vector2(5, 0), 2)
+        expect(s.intersect(c)).toBe(true)
+      })
 
-  test("intersect with Circle - tangent", () => {
-    const a = new Vector2(0, 0)
-    const b = new Vector2(10, 0)
-    const s = new Segment(a, b)
-    const c = new Circle(new Vector2(5, 2), 2)
-    expect(s.intersect(c)).toBe(true) // tangent
-  })
+      test("no intersect test", () => {
+        const a = new Vector2(0, 0)
+        const b = new Vector2(10, 0)
+        const s = new Segment(a, b)
+        const c = new Circle(new Vector2(5, 5), 2)
+        expect(s.intersect(c)).toBe(false)
+      })
 
-  test("intersect throws on invalid type", () => {
-    const a = new Vector2(0, 0)
-    const b = new Vector2(1, 1)
-    const s = new Segment(a, b)
-    // @ts-ignore intentionally wrong type
-    expect(() => s.intersect({})).toThrow()
+      test("tangent", () => {
+        const a = new Vector2(0, 0)
+        const b = new Vector2(10, 0)
+        const s = new Segment(a, b)
+        const c = new Circle(new Vector2(5, 2), 2)
+        expect(s.intersect(c)).toBe(true) // tangent
+      })
+    })
+
+    describe("segment", () => {
+      test("no intersect test", () => {
+        const s2 = new Segment(
+          new Vector2(10,-1),
+          new Vector2(10,1)
+        )
+        const s1 = new Segment(
+          new Vector2(-1,0),
+          new Vector2(1,0)
+        )
+
+        expect(s2.intersect(s1)).toBe(false)
+      })
+
+      test("90deg intersect", () => {
+        const s2 = new Segment(
+          new Vector2(0,-1),
+          new Vector2(0,1)
+        )
+        const s1 = new Segment(
+          new Vector2(-1,0),
+          new Vector2(1,0)
+        )
+
+        expect(s2.intersect(s1)).toBe(true)
+      })
+
+      test("point intersect", () => {
+        const s1 = new Segment(new Vector2(), new Vector2(0, 1))
+        const s2 = new Segment(new Vector2(), new Vector2(0, -1))
+
+        expect(s1.intersect(s2)).toBe(true)
+      })
+
+      test("paralelle no intersect", () => {
+        const s1 = new Segment(new Vector2(0,0), new Vector2(0, 1))
+        const s2 = new Segment(new Vector2(1,0), new Vector2(1, 1))
+
+        expect(s1.intersect(s2)).toBe(false)
+      })
+
+      test("paralelle intersect", () => {
+        const s1 = new Segment(new Vector2(0,0), new Vector2(0, 1))
+        const s2 = new Segment(new Vector2(0,.5), new Vector2(0, 1.5))
+
+        expect(s1.intersect(s2)).toBe(true)
+      })
+
+      test("self intersect", () => {
+        const s1 = new Segment(new Vector2(0,0), new Vector2(0, 1))
+
+        expect(s1.intersect(s1)).toBe(true)
+      })
+
+
+    })
+
+    test("intersect throws on invalid type", () => {
+      const a = new Vector2(0, 0)
+      const b = new Vector2(1, 1)
+      const s = new Segment(a, b)
+      // @ts-ignore intentionally wrong type
+      expect(() => s.intersect({})).toThrow()
+    })
+
+
   })
 })
