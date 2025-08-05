6ae9ee12f1c500451cb91701d0031719c624297d
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-08-05 07:48:26 +0200

    feat(pong-shared): improve intersect functions

--- modules/pong/shared/srcs/math/Segment.ts
+++ modules/pong/shared/srcs/math/Segment.ts
@@ -1,5 +1,6 @@
 import { Vector2 } from "./Vector2"
 import { Circle } from "./shapes/Circle"
+import { Polygon } from "./shapes/Polygon"
 
 export class Segment {
 	constructor(
@@ -11,13 +12,48 @@ export class Segment {
 		return [this.p1, this.p2]
 	}
 
+	getP1() { return this.p1 }
+	getP2() { return this.p2 }
+
 	intersect(other: Circle): boolean
-	intersect(other: Circle): boolean {
+	intersect(other: Segment): boolean
+	intersect(other: Polygon): boolean
+	intersect(other: Circle | Segment | Polygon): boolean {
 		if (other instanceof Circle) {
 			return this.intersectCircle(other)
-		} else {
-			throw "invalid Type in segment intersect"
+		} else if (other instanceof Segment) {
+			return this.intersectSeg(other)
+		} else if (other instanceof Polygon) {
+			return other.intersect(this)
 		}
+		throw "invalid Type in segment intersect"
+		
+	}
+
+	private intersectSeg(other: Segment): boolean {
+		function direction(p: Vector2, q: Vector2, r:Vector2): number {
+			return Vector2.cross(Vector2.subtract(q, p), Vector2.subtract(r, p))
+		}
+
+		const [p3, p4] = other.getPoints()
+
+		const d1 = direction(p3, p4, this.p1)
+		const d2 = direction(p3, p4, this.p2)
+		const d3 = direction(this.p1, this.p2, p3)
+		const d4 = direction(this.p1, this.p2, p4)
+
+		if (((d1 > 0 && d2 < 0) || (d1 < 0 && d2 > 0)) &&
+			((d3 > 0 && d4 < 0) || (d3 < 0 && d4 > 0))) {
+			return true
+		}
+
+		if (d1 === 0 && Segment.pointIsOnSeg(p3, p4, this.p1)) return true
+		if (d2 === 0 && Segment.pointIsOnSeg(p3, p4, this.p2)) return true
+		if (d3 === 0 && Segment.pointIsOnSeg(this.p1, this.p2, p3)) return true
+		if (d4 === 0 && Segment.pointIsOnSeg(this.p1, this.p2, p4)) return true
+
+		return false
+
 	}
 
 	private intersectCircle(other: Circle): boolean {
@@ -33,4 +69,28 @@ export class Segment {
 
 		return distToCircle <= other.getRad()
 	}
+
+	static pointIsOnSeg(p1: Vector2, p2: Vector2, point: Vector2): boolean {
+		const crossProduct = (point.getY() - p1.getY()) * (p2.getX() - p1.getX()) -
+							 (point.getX() - p1.getX()) * (p2.getY() - p1.getY());
+		
+		if (Math.abs(crossProduct) > Number.EPSILON) {
+			return false;
+		}
+		
+		return point.getX() >= Math.min(p1.getX(), p2.getX()) &&
+			   point.getX() <= Math.max(p1.getX(), p2.getX()) &&
+			   point.getY() >= Math.min(p1.getY(), p2.getY()) &&
+			   point.getY() <= Math.max(p1.getY(), p2.getY());
+
+	}
+
+	static containPoint(s: Segment, p: Vector2) {
+		return Segment.pointIsOnSeg(s.getP1(), s.getP2(), p)
+	}
+	
+	contain(p: Vector2): boolean {
+		return Segment.pointIsOnSeg(this.p1, this.p2, p)
+	}
+
 }
--- modules/pong/shared/srcs/math/shapes/Polygon.ts
+++ modules/pong/shared/srcs/math/shapes/Polygon.ts
@@ -16,31 +16,36 @@ export class Polygon {
 		}
 	}
 
-
-
 	intersect(other: Circle): boolean
+	intersect(other: Polygon): boolean
 	intersect(other: Ray): boolean
+	intersect(other: Segment): boolean
 
-	intersect(other: Circle | Ray): boolean {
+	intersect(other: Circle | Ray | Polygon | Segment): boolean {
 		if (other instanceof Circle) {
 			return this.intersectCircle(other)
 		} else if (other instanceof Ray) {
 			return this.intersectRay(other)
+		} else if (other instanceof Polygon) {
+			return this.intersectPolygon(other)
+		} else if (other instanceof Segment) {
+			return this.intersectSeg(other)
 		}
-		throw "Invalid intersect type expected: Circle | Ray"
+		throw "Invalid intersect"
 	}
 
 	private intersectCircle(other: Circle): boolean {
-   		for (const seg of this.segments) {
+		for (const seg of this.segments) {
 			if (seg.intersect(other)) {
 				return true
 			}
 		}
-		return false
+		
+		return this.containsPoint(other.getPos())
 	}
 
 	private intersectRay(other: Ray): boolean {
-   		for (const seg of this.segments) {
+		for (const seg of this.segments) {
 			if (other.intersect(seg)) {
 				return true
 			}
@@ -48,4 +53,62 @@ export class Polygon {
 		return false
 	}
 
+	private intersectPolygon(other: Polygon): boolean {
+		
+		for (const seg1 of this.segments) {
+			for (const seg2 of other.segments) {
+				if (seg1.intersect(seg2)) {
+					return true
+				}
+			}
+		}
+
+		
+		if (this.containsPoint(other.segments[0].getP1())) {
+			return true
+		}
+		if (other.containsPoint(this.segments[0].getP1())) {
+			return true
+		}
+
+		return false
+	}
+
+	containsPoint(point: Vector2): boolean {
+		let inside = false;
+		const points: Vector2[] = this.segments.map((e) => e.getP1());
+		
+		for (const seg of this.segments) {
+			if (seg.contain(point)) {
+				return true;
+			}
+		}
+
+		for (let i = 0, j = points.length - 1; i < points.length; j = i++) {
+			const pi = points[i];
+			const pj = points[j];
+
+			if (((pi.getY() > point.getY()) !== (pj.getY() > point.getY())) &&
+				(point.getX() < (pj.getX() - pi.getX()) * (point.getY() - pi.getY()) /
+				 (pj.getY() - pi.getY()) + pi.getX())) {
+				inside = !inside;
+			}
+		}
+
+		return inside;
+	}
+
+	private intersectSeg(other: Segment) {
+		if (this.containsPoint(other.getP1()) || this.containsPoint(other.getP2())) {
+			return true
+		}
+
+		for (const seg of this.segments) {
+			if (seg.intersect(other)) {
+				return true;
+			}
+		}
+		return false
+	}
+	
 }
