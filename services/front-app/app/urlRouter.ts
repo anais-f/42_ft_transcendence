fc7aac1cb494282ab1339a30bb00b07b593bf24a
anfichet <anfichet@student.42lyon.fr>
2025-08-08 16:19:07 +0200

    feat: simple SPA without CSS
    2 pages to test and a canva for the pong game

--- services/front-app/app/urlRouter.ts
+++ services/front-app/app/urlRouter.ts
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 	const contentDiv = document.getElementById('content');
+	const dynamicMenu = document.getElementById('menu');
 
 	const pages = {
 		home: `
@@ -15,9 +16,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	window.addEventListener('load', function () {
 		let i = 0;
 		for (let key in pages) {
-			let li = document.createElement('li');
-
+			let li = document.createElement('li')
+			li.innerHTML = `<a href="#${key}">${key}</a>`
+			dynamicMenu.appendChild(li);
 		}
 	})
 
+	window.addEventListener('hashchange', () => {
+		loadPage(window.location.hash);
+	})
+
+	function loadPage(hash) {
+		const page = hash ? hash.replace('#', '') : 'home';
+		contentDiv.innerHTML = pages[page] || pages.home
+	}
+
+	loadPage(window.location.hash)
 })
