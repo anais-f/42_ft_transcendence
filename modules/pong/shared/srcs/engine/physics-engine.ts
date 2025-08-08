0fd53ab6e51e6dd539f611835444af508ace5629
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-08-08 15:23:24 +0200

    feat: add physics-engine skeleton

--- modules/pong/shared/srcs/engine/physics-engine.ts
+++ modules/pong/shared/srcs/engine/physics-engine.ts
@@ -1,35 +1,17 @@
 import { Shape } from "../math/shapes/Shape"
 
-enum EngineState {
-	Running,
-	Paused
-}
-
 export class PhysicsEngine {
-    private TPS: number
-    private isRunning: boolean = false
-    private totalTick: number = 0
-    private currentState: EngineState
     private objs: Shape[] = []
 
-    public constructor(TPS: number) {
-        this.TPS = TPS
-        this.currentState = EngineState.Paused
+    public constructor() {
     }
 
     public addObj(obj: Shape) {
         this.objs.push(obj)
     }
 
-    public setState(state: EngineState) {
-        switch (state) {
-            case EngineState.Running:
-                this.isRunning = true
-                break
-            case EngineState.Paused:
-                this.isRunning = false
-                break
-        }
+    public playTick() {
+        // TODO: implement
     }
 }
 
