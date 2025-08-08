e0e38d5e74ce07ae328d7672c5c9f5fb58107611
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-08-08 15:23:42 +0200

    feat: add pad skeleton

--- modules/pong/shared/srcs/engine/objects/pad.ts
+++ modules/pong/shared/srcs/engine/objects/pad.ts
@@ -1,15 +1,15 @@
 import { Shape } from "../../math/shapes/Shape"
 
-class pad {
+export class pad {
     private hitbox: Shape
 
     public constructor(hitbox: Shape) {
         this.hitbox = hitbox
     }
 
-    public intersect(objs: Shape) {
-        for (const obj in objs) {
-            if (obj.intersect()) return true
+    public intersect(objs: Shape[]) {
+        for (const obj of objs) {
+            if (obj.intersect(this.hitbox)) return true
         }
         return false
     }
