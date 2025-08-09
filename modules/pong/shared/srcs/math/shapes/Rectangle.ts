c235f06230a61f858029b0f988b8e9b42d3ef655
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-08-09 20:52:27 +0200

    feat(pong-math): add get<Min/Max><X/Y> methode in Rectangle class

--- modules/pong/shared/srcs/math/shapes/Rectangle.ts
+++ modules/pong/shared/srcs/math/shapes/Rectangle.ts
@@ -1,13 +1,6 @@
 import { Vector2 } from "../Vector2"
 import { Polygon } from "./Polygon";
 
-//TODO : add function for:
-// 	  const center = o.getPos();
-//    const rad = o.getRad();
-//    const minX = this.rect.getSegment()[0].getP1().getX(); // origin.x
-//    const minY = this.rect.getSegment()[0].getP1().getY(); // origin.y
-//    const maxX = this.rect.getSegment()[1].getP1().getX(); // topRight.x
-//    const maxY = this.rect.getSegment()[2].getP1().getY(); // bottomRight.y
 export class Rectangle extends Polygon {
 	constructor(origin: Vector2, dimensions: Vector2) {
 		const topRight = new Vector2(origin.getX() + dimensions.getX(), origin.getY());
@@ -15,6 +8,22 @@ export class Rectangle extends Polygon {
 		const bottomRight = new Vector2(origin.getX() + dimensions.getX(), origin.getY() + dimensions.getY());
 
 		super([origin, topRight, bottomRight, bottomLeft]);
+	}
+
+	public getMinX() {
+		return this.getSegment()[0].getP1().getX()
+	}
+
+	public getMinY() {
+		return this.getSegment()[0].getP1().getY()
+	}
+
+	public getMaxX() {
+		return this.getSegment()[1].getP1().getX()
+	}
+	
+	public getMaxY() {
+		return this.getSegment()[2].getP1().getY()
 	}
 }
 
