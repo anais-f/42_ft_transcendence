c53d771b55ae733df08681ffb092369c0c209915
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-08-25 20:34:21 +0200

    feat: improve PongObject class
    - add static clone methode
    - add clone methode
    - add velocity

--- packages/pong-shared/srcs/engine/objects/PongObject.ts
+++ packages/pong-shared/srcs/engine/objects/PongObject.ts
@@ -3,19 +3,36 @@ import { Vector2 } from '../../math/Vector2'
 
 export class PongObject {
 	private origin: Vector2
+	private velocity: Vector2
 	private hitbox: Shape[] = []
-	constructor(hitbox: Shape | Shape[], origin: Vector2 = new Vector2()) {
+
+	constructor(hitbox: Shape | Shape[], origin: Vector2 = new Vector2(), velocity: Vector2 = new Vector2()) {
 		if (Array.isArray(hitbox)) {
 			this.hitbox = hitbox
 		} else {
 			this.hitbox = [hitbox]
 		}
 		this.origin = origin
+		this.velocity = velocity
 	}
+
+	getVelocity(): Vector2 {
+		return this.velocity
+	}
+
 	getHitbox(): Shape[] {
 		return this.hitbox
 	}
+
 	getOrigin(): Vector2 {
 		return this.origin
+	}
+
+	static clone(obj: PongObject) {
+		return new PongObject(obj.getHitbox(), obj.getOrigin(), obj.getVelocity())
+	}
+	
+	clone() {
+		return PongObject.clone(this)
 	}
 }
