98c7d151179d8b98c012eb357471408515558151
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-08-26 03:26:16 +0200

    feat(circle): intersect Circle with Ray

--- packages/pong-shared/srcs/math/shapes/Circle.ts
+++ packages/pong-shared/srcs/math/shapes/Circle.ts
@@ -1,5 +1,7 @@
+import { Ray } from '../Ray'
 import { Segment } from '../Segment'
 import { Vector2 } from '../Vector2'
+import { Polygon } from './Polygon'
 import { Shape } from './Shape'
 
 export class Circle extends Shape {
@@ -26,21 +28,58 @@ export class Circle extends Shape {
 
 	setRad(rad: number): void {
 		if (rad <= 0) {
-			throw 'invlalid Radius'
+			throw 'Invlalid Radius'
 		}
 		this.rad = rad
 	}
 
 	intersect(other: Segment): boolean
 	intersect(other: Circle): boolean
-	intersect(other: Segment | Circle): boolean {
+	intersect(other: Ray): boolean
+	intersect(other: Polygon): boolean
+
+	intersect(other: Segment | Ray | Polygon | Circle): boolean {
 		if (other instanceof Segment) {
 			return other.intersect(this)
 		} else if (other instanceof Circle) {
 			return this.intersectCircle(other)
-		} else {
-			throw 'invalid Type in circle intersect'
+		} else if (other instanceof Ray) {
+			return this.intersectRay(other)
+		} else if (other instanceof Polygon) {
+			return other.intersect(this)
 		}
+		throw 'Invalid type'
+	}
+
+	private intersectRay(other: Ray): boolean {
+		const rayOrigin = other.getOrigin()
+		const rayDirection = other.getDirection()
+
+		const squaredDistanceToCenter = Vector2.squaredDist(
+			rayOrigin,
+			this.getPos()
+		)
+		const squaredRadius = this.getRad() * this.getRad()
+
+		if (squaredDistanceToCenter <= squaredRadius) {
+			return true
+		}
+
+		const toCircle = Vector2.subtract(this.getPos(), rayOrigin)
+		const projection = Vector2.dot(toCircle, rayDirection)
+
+		if (projection < 0) {
+			return false
+		}
+
+		const scaledDirection = Vector2.multiply(rayDirection, projection)
+		const closestPoint = Vector2.add(rayOrigin, scaledDirection)
+		const squaredDistanceToClosest = Vector2.squaredDist(
+			closestPoint,
+			this.getPos()
+		)
+
+		return squaredDistanceToClosest <= squaredRadius
 	}
 
 	private intersectCircle(other: Circle): boolean {
