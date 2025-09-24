45b78f110658b734dae4ffd8ccfbb75dea1402b1
anfichet <anfichet@student.42lyon.fr>
2025-09-24 15:58:10 +0200

    fix: symtax typo

--- services/friends/app/index.ts
+++ services/friends/app/index.ts
@@ -9,7 +9,7 @@ const start = async () => {
 		await app.listen({ port: 3000, host: '0.0.0.0' })
 		console.log('Listening on port 3000')
 	} catch (err) {
-		console.error('Error strating server: ', err)
+		console.error('Error starting server: ', err)
 		process.exit(1)
 	}
 }
