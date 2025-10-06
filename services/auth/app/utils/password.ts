93d2bd78a1e17b03dda6815b4198fe2f8cbd9823
Michel Juffard <mjuffard@z1r2p3.42lyon.fr>
2025-10-06 23:35:05 +0200

    feat: create function for password hashing

---
+++ services/auth/app/utils/password.ts
@@ -0,0 +1,14 @@
+import argon2, { argon2id } from 'argon2'
+
+export async function hashPassword(password: string) {
+  return argon2.hash(password, {
+    type: argon2id,
+    memoryCost: 65536,
+    timeCost: 2,
+    parallelism: 1
+  })
+}
+
+export async function verifyPassword(hash: string, password: string) {
+  return argon2.verify(hash, password)
+}
