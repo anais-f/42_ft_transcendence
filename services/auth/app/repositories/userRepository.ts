19221c1640d2ae6aa6d87a3c7344136af9e682ac
Michel Juffard <mjuffard@z1r6p3.42lyon.fr>
2025-10-07 22:48:40 +0200

    style: use make format

--- services/auth/app/repositories/userRepository.ts
+++ services/auth/app/repositories/userRepository.ts
@@ -4,34 +4,38 @@ import { UserRow } from '../models/userModels.js'
 const db = () => getDb()
 
 export function createUser(username: string, passwordHash: string) {
-  const stmt = db().prepare('INSERT INTO users (username, password) VALUES (?, ?)')
-  stmt.run(username, passwordHash)
+	const stmt = db().prepare(
+		'INSERT INTO users (username, password) VALUES (?, ?)'
+	)
+	stmt.run(username, passwordHash)
 }
 
-export function findUserByUsername(username: string)
-{
-  const stmt = db().prepare('SELECT * FROM users WHERE username = ?')
-  return stmt.get(username) as UserRow | undefined
+export function findUserByUsername(username: string) {
+	const stmt = db().prepare('SELECT * FROM users WHERE username = ?')
+	return stmt.get(username) as UserRow | undefined
 }
 
-export function findPublicUserByUsername(username: string)
-{
-  const stmt = db().prepare('SELECT id_user, username FROM users WHERE username = ?')
-  return stmt.get(username) as { id: number; username: string} | undefined
+export function findPublicUserByUsername(username: string) {
+	const stmt = db().prepare(
+		'SELECT id_user, username FROM users WHERE username = ?'
+	)
+	return stmt.get(username) as { id: number; username: string } | undefined
 }
 
 export function findPublicUserById(id: number) {
-  const stmt = db().prepare('SELECT id_user, username FROM users WHERE id_user = ?')
-  return stmt.get(id) as { id: number; username: string } | undefined
+	const stmt = db().prepare(
+		'SELECT id_user, username FROM users WHERE id_user = ?'
+	)
+	return stmt.get(id) as { id: number; username: string } | undefined
 }
 
 export function listPublicUsers() {
-  const stmt = db().prepare('SELECT id_user, username FROM users')
-  return stmt.all() as { id: number; username: string; }[]
+	const stmt = db().prepare('SELECT id_user, username FROM users')
+	return stmt.all() as { id: number; username: string }[]
 }
 
 export function deleteUserById(id: number) {
-  const stmt = db().prepare('DELETE FROM users WHERE id_user = ?')
-  const info = stmt.run(id)
-  return info.changes > 0
+	const stmt = db().prepare('DELETE FROM users WHERE id_user = ?')
+	const info = stmt.run(id)
+	return info.changes > 0
 }
