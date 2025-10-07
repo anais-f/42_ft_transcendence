9a1a8954eac7793bf01680d493def6e5e942c61c
Michel Juffard <mjuffard@z1r2p3.42lyon.fr>
2025-10-07 02:35:42 +0200

    feat: add users zod schema and use it in userController

---
+++ services/auth/app/models/usersDTO.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+const USERNAME_REGEX = /^[A-Za-z0-9_-]{4,16}$/
+
+export const UsernameSchema = z.string()
+  .min(4)
+  .max(16)
+  .regex(USERNAME_REGEX, 'Invalid username')
+
+export const PublicUserSchema = z.object({
+	id: z.number().positive().min(1),
+	username: UsernameSchema
+}).strict()
+
+export const  PublicUserListSchema = z.object({
+	users: z.array(PublicUserSchema)
+}).strict()
