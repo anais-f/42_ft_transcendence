d349dea497ed7b7eabab988216aef434b0bde61c
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-10-08 04:40:39 +0200

    refactor: suppression of the world class because it is too complex

--- packages/pong-shared/srcs/engine/physics-engine.ts
+++ packages/pong-shared/srcs/engine/physics-engine.ts
@@ -1,13 +1,9 @@
-import { WorldRect } from './map/worldType/WorldRect'
 import { PongObject } from './objects/PongObject'
-import { AWorld } from './map/AWorld'
 
 export class PhysicsEngine {
 	private objs: PongObject[] = []
-	private world: AWorld
 
 	public constructor() {
-		this.world = new WorldRect()
 	}
 
 	public addObj(obj: PongObject) {
