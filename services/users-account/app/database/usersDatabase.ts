c4d5ca1ed9f6932441fea0e532c1fcb88be7cb4c
michjuffard <juffard.michel@gmail.com>
2025-10-10 01:36:49 +0200

    build: create volumes for db

--- services/users-account/app/database/usersDatabase.ts
+++ services/users-account/app/database/usersDatabase.ts
@@ -1,7 +1,7 @@
 import DatabaseConstructor from 'better-sqlite3'
 import type { Database } from 'better-sqlite3'
 
-export const db: Database = new DatabaseConstructor('./db-users.sqlite')
+export const db: Database = new DatabaseConstructor('/data/db-users.sqlite')
 
 db.exec(`  CREATE TABLE IF NOT EXISTS users (
     id_user INTEGER PRIMARY KEY,
