ab0d2ec126312990ae47e9fd5d44d1b58438ad4a
anais <anais.code@proton.me>
2025-10-12 15:07:55 +0200

    feat: common schemas Zod between auth and users services

---
+++ services/common/DTO/ResponseSchema.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+
+
+export const SuccessResponseSchema = z.object({
+	success: z.literal(true),
+	message: z.string().optional()
+})
+
+export const ErrorResponseSchema = z
+	.object({
+		success: z.boolean().optional(),
+		error: z.string()
+	})
+	.strict()
---
+++ services/common/DTO/authSchema.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod'
+import { UsernameSchema } from './usersSchema'
+
+export const RegisterSchema = z
+	.object({
+		username: UsernameSchema,
+		password: z.string().min(8).max(128)
+	})
+	.strict()
+
+export const LoginSchema = z
+	.object({
+		username: UsernameSchema,
+		password: z.string().min(8).max(128)
+	})
+	.strict()
