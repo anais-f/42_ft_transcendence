633d8912ae7173f30882327b55410c834b7f9a14
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-10-20 13:54:44 +0200

    refactor: rename pad -> PongPad
     - rename class
     - rename the class file

---
+++ packages/pong-shared/srcs/engine/objects/PongPad.ts
@@ -0,0 +1,42 @@
+import { Vector2 } from '../../math/Vector2'
+import { PongObject } from './PongObject'
+
+export enum PadDirection {
+	Up,
+	Down
+}
+
+export class PongPad {
+	private player: number
+	private obj: PongObject
+	public constructor(player: number, obj: PongObject) {
+		this.player = player
+		this.obj = obj
+	}
+
+	public getPlayer(): number {
+		return this.player
+	}
+
+	public getObjs(): PongObject {
+		return this.obj
+	}
+
+	public move(dir: PadDirection, incr: number, border: PongObject[]) {
+		const pos = this.obj.getOrigin()
+		switch (dir){
+			case PadDirection.Up:
+				this.obj.setOrigin(Vector2.add(pos, new Vector2(0, incr)))
+				break
+			case PadDirection.Down:
+				this.obj.setOrigin(Vector2.add(pos, new Vector2(0, incr)))
+				break
+		}
+		for (const o of border) {
+			if (o.intersect(this.obj)) {
+				this.obj.setOrigin(pos)
+				break
+			}
+		}
+	}
+}
