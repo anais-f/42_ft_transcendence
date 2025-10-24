a8f97853748b3c8f7118a012e23ab087d65d6c75
anfichet <anfichet@student.42lyon.fr>
2025-10-24 21:21:36 +0200

    fix: test jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,22 @@
+import path from 'node:path'
 import type { Config } from '@jest/types'
+
+const repoRoot = path.resolve(__dirname)
+
+// Map to sources (recommended: no prebuild required)
+const COMMON_SRC = path.join(repoRoot, 'packages/common/src/index.ts')
 
 const commonConfig: Partial<Config.InitialProjectOptions> = {
 	preset: 'ts-jest/presets/default-esm',
 	testEnvironment: 'node',
 	extensionsToTreatAsEsm: ['.ts'],
+  rootDir: './',
 	moduleNameMapper: {
 		'^(\\.{1,2}/.*)\\.js$': '$1',
 		'^@packages/(.*)$': '<rootDir>/packages/$1',
 		'^@services/(.*)$': '<rootDir>/services/$1',
-		'^@ft_transcendence/common$':
-			'<rootDir>/../../../packages/common/dist/index.js'
-	},
+    '^@ft_transcendence/common$': COMMON_SRC,
+  },
 	transform: {
 		'^.+\\.tsx?$': [
 			'ts-jest',
