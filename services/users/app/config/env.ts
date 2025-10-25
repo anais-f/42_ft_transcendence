be3c4e073200359a0c17ae633c8198a93a37ce1e
michjuffard <juffard.michel@gmail.com>
2025-10-26 01:41:07 +0200

    feat: create env variable for switching mode

--- services/users/app/config/env.ts
+++ services/users/app/config/env.ts
@@ -3,5 +3,6 @@ import 'dotenv/config'
 export const ENV = {
   PORT: parseInt(process.env.PORT ?? '3002'),
   DB_PATH: process.env.DB_PATH ?? './db-users.sqlite',
-  AUTH_API_BASE_URL: process.env.AUTH_API_BASE_URL ?? 'http://localhost:3001'
+  AUTH_API_BASE_URL: process.env.AUTH_API_BASE_URL ?? 'http://localhost:3001',
+  OPEN_API_FILE: process.env.OPEN_API_FILE ?? '../../../packages/common/openapiDTO.json'
 }
