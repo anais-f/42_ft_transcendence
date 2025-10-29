1036959e27d0e125fcf623af769014eaa66ef250
michjuffard <juffard.michel@gmail.com>
2025-10-29 17:48:11 +0100

    refactor: remove useless command

--- services/auth/app/controllers/userController.ts
+++ services/auth/app/controllers/userController.ts
@@ -1,5 +1,5 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
-import { getPublicUsers, getPublicUser } from '../usecases/user.js'
+import { listPublicUsers as getPublicUsers, findPublicUserById as getPublicUser } from '../repositories/userRepository.js'
 import {
 	PublicUserAuthSchema,
 	PublicUserListAuthSchema
