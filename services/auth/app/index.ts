174e20eb110d37104de21dd470efbdbc723628b4
michjuffard <juffard.michel@gmail.com>
2025-10-29 04:32:05 +0100

    feat: create some wrappers for prometheus

--- services/auth/app/index.ts
+++ services/auth/app/index.ts
@@ -10,7 +10,8 @@ import { registerRoutes } from './routes/registerRoutes.js'
 import Swagger from '@fastify/swagger'
 import SwaggerUI from '@fastify/swagger-ui'
 import fs from 'fs'
-import path from 'path'
+import metricPlugin from 'fastify-metrics'
+import { httpRequestCounter, responseTimeHistogram } from '@ft_transcendence/common'
 
 const app = Fastify({
 	logger: true
@@ -19,6 +20,29 @@ const app = Fastify({
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
+app.addHook('onRequest', (request, reply, done) => {
+	(request as any).startTime = process.hrtime();
+	done();
+});
+
+app.addHook('onResponse', (request, reply) => {
+	httpRequestCounter.inc({
+		method: request.method,
+		route: request.url,
+		status_code: reply.statusCode
+	});
+	const startTime = (request as any).startTime;
+ 	if (startTime) {
+		const diff = process.hrtime(startTime);
+		const responseTimeInSeconds = diff[0] + diff[1] / 1e9;
+		responseTimeHistogram.observe({
+		method: request.method,
+		route: request.url,
+		status_code: reply.statusCode
+	}, responseTimeInSeconds);
+  } 
+});
+
 async function runServer() {
 	console.log('Starting Auth service...')
 	await runMigrations()
@@ -26,11 +50,11 @@ async function runServer() {
 	// Load OpenAPI schemas from file
 	const openapiSwagger = JSON.parse(
 		fs.readFileSync(
-			path.join(process.cwd(), process.env.OPEN_API_FILE as string),
+			process.env.OPEN_API_FILE as string,
 			'utf-8'
 		)
 	)
-
+	await app.register(metricPlugin.default, { endpoint: '/metrics' });
 	// Configure Swagger to use the loaded schemas
 	await app.register(Swagger as any, {
 		openapi: {
--- services/users/app/index.ts
+++ services/users/app/index.ts
@@ -13,9 +13,10 @@ import {
 } from 'fastify-type-provider-zod'
 import { usersRoutes } from './routes/usersRoutes.js'
 import { UsersServices } from './usecases/usersServices.js'
+import metricPlugin from 'fastify-metrics'
+import { httpRequestCounter, responseTimeHistogram } from '@ft_transcendence/common'
 
 const OPENAPI_FILE = path.join(
-	process.cwd(),
 	process.env.OPEN_API_FILE as string
 )
 const SWAGGER_TITTLE = 'API for Users Service'
@@ -64,7 +65,30 @@ async function initializeUsers(): Promise<void> {
 
 export async function start(): Promise<void> {
 	const app = createApp()
+	app.addHook('onRequest', (request, reply, done) => {
+		(request as any).startTime = process.hrtime();
+		done();
+	});
+
+	app.addHook('onResponse', (request, reply) => {
+		httpRequestCounter.inc({
+			method: request.method,
+			route: request.url,
+			status_code: reply.statusCode
+		});
+		const startTime = (request as any).startTime;
+		if (startTime) {
+			const diff = process.hrtime(startTime);
+			const responseTimeInSeconds = diff[0] + diff[1] / 1e9;
+			responseTimeHistogram.observe({
+			method: request.method,
+			route: request.url,
+			status_code: reply.statusCode
+		}, responseTimeInSeconds);
+	} 
+	});
 	try {
+		await app.register(metricPlugin.default, { endpoint: '/metrics' });
 		await app.ready()
 		await dumpOpenAPISchema(app)
 		await initializeUsers()
