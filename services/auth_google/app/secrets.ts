0e015ddccab7c41d3fca6c56acff21482e6e6c12
michjuffard <juffard.michel@gmail.com>
2025-11-11 16:06:20 +0100

    style: use make format

--- services/auth_google/app/secrets.ts
+++ services/auth_google/app/secrets.ts
@@ -4,9 +4,7 @@ export function getGoogleCredentials(): {
 	clientId?: string
 	clientSecret?: string
 } {
-	const clientId =
-		process.env.GOOGLE_CLIENT_ID
-	const clientSecret =
-		process.env.GOOGLE_CLIENT_SECRET
+	const clientId = process.env.GOOGLE_CLIENT_ID
+	const clientSecret = process.env.GOOGLE_CLIENT_SECRET
 	return { clientId, clientSecret }
 }
