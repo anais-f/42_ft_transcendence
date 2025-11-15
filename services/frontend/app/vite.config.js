62bf63b9259ee1c9b3828709e4cde6c2e34e7b52
Acancel <acancel@student.42lyon.fr>
2025-11-15 15:25:46 +0100

    chore(frontend/app/): Update style and vite.conf

--- services/frontend/app/vite.config.js
+++ services/frontend/app/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
 	base: './',
-	build: { outDir: 'dist', emptyOutDir: true }
+	build: { outDir: 'dist', emptyOutDir: true },
+	// plugins: [
+	// 	tailwindcss(),
+	// ],
 })
