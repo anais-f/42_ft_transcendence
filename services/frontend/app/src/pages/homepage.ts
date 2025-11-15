d2218c8fdbec6753378160f413a2283866792219
Acancel <acancel@student.42lyon.fr>
2025-11-15 15:29:41 +0100

    feat(frontend/app/): Update static Homepage

--- services/frontend/app/src/pages/homepage.ts
+++ services/frontend/app/src/pages/homepage.ts
@@ -1,6 +1,25 @@
 export const HomePage = (): string => /*html*/`
-  <div class="p-5 text-green-300">
-    <h1>Welcome to Transcendence</h1>
-    <p>This is a single-page application example.</p>
-  </div>
+<header>
+  <!-- ? -->
+</header>
+<body>
+    <h1 class="text-8xl text-center text-orange-400 bg-[url(/images/pong.avif)] bg-no-repeat bg-center py-48">
+      ft_transcendence</h1>
+    <div class="flex place-content-center">
+      <a href="#register">
+        <button class="bg-green-800 p-3 m-5 rounded-lg hover:bg-green-300 hover:text-black">Sign in</button>
+      </a>
+      <a href="#wales">
+        <button class="bg-blue-800  m-5 p-3 rounded-lg hover:bg-sky-300 hover:text-black">Sign up</button>
+      </a>
+        <a href="#me" class="flex justify-between">
+          <button class=" bg-gray-100 place-center m-5 rounded-lg p-3 px-4 text-black hover:bg-gray-400">
+            <img src="/images/Google_logo_svg.webp" alt="Google_logo" class="size-7 inline -ml-3">
+            Continue with Google</button>
+        </a>
+    </div>
+</body>
+		<footer class="border-t border-gray-800 text-center text-s text-gray-400 my-24 py-8">
+			© 2025 ft_Transcendence — All rights reserved
+		</footer>
 `
