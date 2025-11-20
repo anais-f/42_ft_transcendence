9dc17f0058e554bd0d4f5358935e05864acf1777
Acancel <acancel@student.42lyon.fr>
2025-11-20 20:05:45 +0100

    feat(service/fontend/app/): for now, this file is used like urlRouter.ts

---
+++ services/frontend/app/src/main.ts
@@ -0,0 +1,42 @@
+import '../style.css'
+import { createButton, createLink, createImage } from "./components/componentsUtils.js";
+import { showPopup } from './components/popUp.js';
+
+const root: HTMLElement = document.getElementById('root') ?? document.body
+
+const link: HTMLAnchorElement = createLink({
+    href: '#home',
+    title: 'HOME_SWEET_HOME',
+    className: 'text-blue-600 hover:text-red-500 text-center',
+    onClick: ((ev: MouseEvent) => {console.log('click')}),
+    imgSrc: '/images/acancel.jpg',
+    imgAlt: 'avatar',
+    imgClass: 'rounded-full w-1/4 aspect-square object-cover border-4 border-gray-400 shadow-xl items-center'
+})
+
+let hide: boolean = true
+
+const button: HTMLButtonElement = createButton({
+    name: 'REGISTER',
+    type: 'button',
+    className: ' bg-gray-300 p-8 self-center rounded-xl border-2 border-gray-400 hover:bg-gray-400 shadow-xl',
+    onClick: ((ev: MouseEvent) => {
+        console.log('clack')
+        const img = createImage({
+            src: '/images/lrio.jpg',
+            alt: 'LRIO'
+        })
+        showPopup(img, {
+            id: 'popup-overlay',
+            className: 'fixed inset-0 z-50 flex items-center justify-center bg-black/50'
+        })
+    })
+})
+
+
+root.appendChild(link)
+root.appendChild(button)
+
+
+
+
