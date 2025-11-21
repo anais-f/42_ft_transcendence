866386d7c3f760eb5447435d47b09c5c4cddddd0
acancel42 <acancel@student.42lyon.fr>
2025-11-21 16:54:05 +0100

    feat(service/frontend/app): Need to talk with my m8 of this devflow...

--- services/frontend/app/src/components/componentsUtils.ts
+++ services/frontend/app/src/components/componentsUtils.ts
@@ -1,78 +1,116 @@
 interface interfaceLink {
-    href: string,
-    title: string,
-    className?: string, //style ie : bg-white, text-black
-    imgSrc?: string,
-    imgAlt?: string,
-    imgClass?: string,
-    onClick?: (ev: MouseEvent) => void
+	href: string
+	title: string
+	className?: string //style ie : bg-white, text-black
+	imgSrc?: string
+	imgAlt?: string
+	imgClass?: string
+	onClick?: (ev: MouseEvent) => void
 }
 
 interface interfaceButton {
-    label?: string,
-    className?: string,
-    name?: string,
-    type?: string,
-    disabled?: boolean,
-    formAction?: string,
-    formEnctype?: string,
-    formMethod?: string,
-    formNoValidate?: boolean,
-    formTarget?: string,
-    onClick?: (ev: MouseEvent) => void
+	label?: string
+	className?: string
+	name?: string
+	type?: string
+	disabled?: boolean
+	formAction?: string
+	formEnctype?: string
+	formMethod?: string
+	formNoValidate?: boolean
+	formTarget?: string
+	onClick?: (ev: MouseEvent) => void
+}
+
+export class Button implements interfaceButton {
+	label?: string
+	className?: string
+	name?: string
+	type?: string
+	disabled?: boolean
+	formAction?: string
+	formEnctype?: string
+	formMethod?: string
+	formNoValidate?: boolean
+	formTarget?: string
+	onClick?: (ev: MouseEvent) => void
+
+	constructor(
+		label?: string,
+		className?: string,
+		name?: string,
+		type?: string,
+		onClick?: (ev: MouseEvent) => void
+	) {
+		this.label = label
+		this.className = className
+		this.name = name
+		this.type = type
+		this.onClick = onClick
+	}
+
+	setClassName(className: string): void {
+		this.className = className
+	}
+
+	addStyle(style: string): void {
+		if (this.className) this.className += ' ' + style
+		else this.className = style
+	}
+
+	addPositioning(positioning: string): void {
+		this.addStyle(positioning)
+	}
 }
 
 interface interfaceImage {
-    src: string,
-    alt: string,
-    title?: string,
-    className?: string,
+	src: string
+	alt: string
+	title?: string
+	className?: string
 }
 
 export function createImage(imgDef: interfaceImage): HTMLImageElement {
-    const newImg: HTMLImageElement = document.createElement('img')
-    newImg.src = imgDef.src
-    newImg.alt = imgDef.alt
-    newImg.title = imgDef.title ?? ''
-    newImg.className = imgDef.className ?? ''
-    return newImg
+	const newImg: HTMLImageElement = document.createElement('img')
+	newImg.src = imgDef.src
+	newImg.alt = imgDef.alt
+	newImg.title = imgDef.title ?? ''
+	newImg.className = imgDef.className ?? ''
+	return newImg
 }
 
 export function createLink(linkDef: interfaceLink): HTMLAnchorElement {
+	const newAnchor: HTMLAnchorElement = document.createElement('a')
 
-    const newAnchor: HTMLAnchorElement = document.createElement('a');
-
-    newAnchor.href = linkDef.href;
-    if (linkDef.className)
-        newAnchor.className = linkDef.className;
-    if (linkDef.onClick)
-        newAnchor.addEventListener('click', linkDef.onClick)
-    if (linkDef.imgSrc)
-    {
-        const img: HTMLImageElement = document.createElement('img');
-        img.src = linkDef.imgSrc;
-        img.alt = linkDef.imgAlt ?? ''
-        if (linkDef.imgClass)
-            img.className = linkDef.imgClass;
-        newAnchor.append(img); // append or prepend ?
-    }
-    const title = document.createElement('span')
-    title.textContent = linkDef.title
-    newAnchor.appendChild(title)
-    return newAnchor;
+	newAnchor.href = linkDef.href
+	if (linkDef.className) newAnchor.className = linkDef.className
+	if (linkDef.onClick) newAnchor.addEventListener('click', linkDef.onClick)
+	if (linkDef.imgSrc) {
+		const img: HTMLImageElement = document.createElement('img')
+		img.src = linkDef.imgSrc
+		img.alt = linkDef.imgAlt ?? ''
+		if (linkDef.imgClass) img.className = linkDef.imgClass
+		newAnchor.append(img) // append or prepend ?
+	}
+	const title = document.createElement('span')
+	title.textContent = linkDef.title
+	newAnchor.appendChild(title)
+	return newAnchor
 }
 
-export function createButton(buttonDef: interfaceButton): HTMLButtonElement {
-    const newButton: HTMLButtonElement = document.createElement('button');
+export function createButton(bProp: interfaceButton): HTMLButtonElement {
+	const newButton: HTMLButtonElement = document.createElement('button')
 
-    newButton.name = buttonDef.name ?? ''
-    newButton.className = buttonDef.className ?? ''
-    if (buttonDef.type === 'submit' || buttonDef.type === 'reset' || buttonDef.type === 'button')
-        newButton.type = buttonDef.type;
-    else
-        newButton.type = 'button'
-    newButton.textContent = buttonDef.label ?? buttonDef.name ?? ''
-    if (buttonDef.onClick)
-        newButton.addEventListener('click', buttonDef.onClick)
-    return newButton;
+	newButton.name = bProp.name ?? ''
+	newButton.className = bProp.className ?? ''
+	if (
+		bProp.type === 'submit' ||
+		bProp.type === 'reset' ||
+		bProp.type === 'button'
+	)
+		newButton.type = bProp.type
+	else newButton.type = 'button'
+	newButton.textContent = bProp.label ?? bProp.name ?? ''
+	if (bProp.onClick) newButton.addEventListener('click', bProp.onClick)
+	return newButton
 }
