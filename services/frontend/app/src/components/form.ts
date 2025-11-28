dbe794302c3f915b1f1f1e881ddf536f54eced54
Acancel <acancel@student.42lyon.fr>
2025-11-28 21:29:55 +0100

    feat(service/fontend/app): update generic style of form

--- services/frontend/app/src/components/form.ts
+++ services/frontend/app/src/components/form.ts
@@ -4,7 +4,7 @@ interface FormProps {
 	action?: string
 	method?: string
 	label?: string[]
-	input?: { id: string; type: string; name: string; required?: boolean }[]
+	input?: { id: string; type: string; name: string; required?: boolean;  separator?: boolean; }[]
 	button?: { text: string; type: 'submit' | 'button'; className?: string; onClick?: (ev: MouseEvent) => void }
 	onSubmit?: (data: FormData) => void
 }
@@ -22,7 +22,7 @@ export function createForm(props: FormProps): HTMLFormElement {
 		if (inputs.length > 0) {
 			inputs.forEach((input, idx) => {
 				const field = document.createElement('div')
-				field.className = 'gap-4'
+				field.className = 'gap-4 flex border border-gray-400'
 				const labelText = labels[idx]
 				if (labelText) {
 					const labelElement = document.createElement('label')
@@ -55,6 +55,8 @@ export function createForm(props: FormProps): HTMLFormElement {
 		const button = document.createElement('button')
 		button.textContent = props.button.text
 		button.type = props.button.type
+		if (props.button.className) 
+			button.className = props.button.className
 		if (props.button.type === 'button' && props.button.onClick)
 			button.addEventListener('click', props.button.onClick)
 
@@ -62,12 +64,12 @@ export function createForm(props: FormProps): HTMLFormElement {
 	}
 
 	if (props.onSubmit) {
-    	const submitHandler = props.onSubmit
-    	form.addEventListener('submit', (ev) => {
-        	ev.preventDefault()
-        	const formData = new FormData(form)
-        	submitHandler(formData)
-    	})
+		const submitHandler = props.onSubmit
+		form.addEventListener('submit', (ev) => {
+			ev.preventDefault()
+			const formData = new FormData(form)
+			submitHandler(formData)
+		})
 	}
 
 	return form
