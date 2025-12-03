b51a79fea136da49059950209ad0f38134f60982
Acancel <acancel@student.42lyon.fr>
2025-12-03 23:53:40 +0100

    feat(service/frontend/app): add date of the day

--- services/frontend/app/src/router.ts
+++ services/frontend/app/src/router.ts
@@ -6,11 +6,25 @@ import { LobbyPage } from "./pages/lobby.js"
 
 document.addEventListener('DOMContentLoaded', () => {
 	const contentDiv = document.getElementById('content');
-
 	if (!contentDiv) {
 		console.error('Missing #content in index.html');
 		return;
 	}
+
+	// Date of the day
+	const dateDiv = document.getElementById('date');
+	if (dateDiv)
+	{
+		dateDiv.textContent = new Date().toLocaleDateString('en-EN', {
+			weekday:'long',
+			day: 'numeric',
+			month:'short',
+			year:'numeric',
+
+		})
+
+	}
+
 	const content: HTMLElement = contentDiv;
 
 	content.innerHTML = HomePage();
