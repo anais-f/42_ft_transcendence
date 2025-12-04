cb603eb75d8e728b01375cb81cc4180e87ee408f
Acancel <acancel@student.42lyon.fr>
2025-12-04 01:26:10 +0100

    feat(service/frontend/app): update home and login page

--- services/frontend/app/src/pages/login.ts
+++ services/frontend/app/src/pages/login.ts
@@ -1,21 +1,76 @@
 
 export const LoginPage = (): string => /*html*/ `
-	<section class="pt-6 h-[100%]">
-		<h1 class="text-5xl font-bold text-center">Transcendence</h1>
-		<div id="img-home">
-			<img src="/images/pong.avif" alt="ping" class="flex-shrink-0 p-10">
+<section class="grid grid-cols-4 gap-11">
+	<div class="col-span-1 flex flex-col items-start">
+		<h1 class="text-2xl py-4">SUBSCRIBE TO OUR NEWSPAPER</h1>
+		<form id="register_form" class="flex flex-col gap-2">
+		<input id="register_username" type="text" name="register_username" class=" px-2 border-b-2 text-xl border-black bg-inherit w-full font-[Birthstone]" placeholder="USERNAME" required>
+		<input id="register_password" type="password" name="register_password" class=" px-2 border-b-2 text-xl border-black bg-inherit w-full font-[Birthstone]" placeholder="PASSWORD" required>
+		<input id="register_conf_password" type="password" name="register_conf_password" class=" px-2 border-b-2 text-xl border-black bg-inherit w-full font-[Birthstone]" placeholder="CONFIRM PASSWORD" required>
+		<button id="register_btn" class="generic_btn mt-4" type="submit">Register</button>
+		<div class="news_paragraph">
+			<h1 class="text-lg pt-4">Title</h1>
+			<p class="text-sm py-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia. Assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia assumenda reprehenderit nesciunt. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia assumenda reprehenderit nesciunt. Eos debitis officia. Assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia assumenda reprehenderit nesciunt. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia assumenda reprehenderit nesciunt.</p>
 		</div>
-		<div id="btn-home" class="grid grid-cols-[1fr_0.1fr_1fr] grid-rows-2 p-4 gap-5 size-fit mx-auto">
-			<button id="btn-signup" class="generic_btn col-start-1 col-end-2 p-2">Sign up</button>
-			<button id="btn-signin" class="generic_btn col-start-3 col-end-4 p-2">Sign in</button>
-			<button id="btn-google" class="generic_btn col-span-3 p-2 px-4 flex justify-center">
-				<img src="/images/l-ggl.webp" alt="Google_logo" class="size-7 inline -ml-3">
-				Continue with Google
-			</button>
+	</div>
+
+	<div class="col-span-1 flex flex-col items-start">
+		<div class="news_paragraph">
+			<p class="text-sm pt-6 pb-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia. Assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat.</p>
 		</div>
-	</section>
+		<img src="/images/image_9.png" alt="mamouth" class="w-full object-cover opacity-50 select-none">
+		<h1 class="text-2xl pt-4 pb-1">RESUME READING</h1>
+		<form id="login_form" class="flex flex-col gap-2">
+		<input id="login_username" type="text" name="login_username" class=" px-2 border-b-2 text-xl border-black bg-inherit w-full font-[Birthstone]" placeholder="USERNAME" required>
+		<input id="login_password" type="password" name="login_password" class=" px-2 border-b-2 text-xl border-black bg-inherit w-full font-[Birthstone]" placeholder="PASSWORD" required>
+		<button id="login_btn" class="generic_btn mt-4" type="submit">Login</button>
+		<div class="news_paragraph">
+			<h1 class="text-lg pt-4">Title</h1>
+			<p class="text-sm py-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores.</p>
+		</div>
+	</div>
+
+	<div class="col-span-1 flex flex-col items-start">
+		<div class="news_paragraph">
+			<p class="text-sm py-6">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia. Assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia assumenda reprehenderit nesciunt. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia assumenda reprehenderit nesciunt.</p>
+		</div>
+		<button id="btn-google" class="generic_btn">
+			<img src="/images/l-ggl.webp" alt="Google_logo" class="size-7 inline -ml-3 -mt-1">
+			Continue with Google
+		</button>
+
+		<!-- <button id="google_btn" type="button" class="generic_btn">Continue with Google</button> -->
+
+		<div class="news_paragraph">
+			<h1 class="text-lg pt-4">Title</h1>
+			<p class="text-sm pb-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi.</p>
+		</div>
+		<img src="/images/image_10.png" alt="AHHHHH !" class="w-full object-cover opacity-50 select-none">
+
+	</div>
+	<div class="col-span-1 flex flex-col items-start">
+		<div class="news_paragraph">
+			<h1 class="text-lg py-2">Title</h1>
+			<p class="text-sm py-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia. Nam perferendis facilis asperiores ea qui voluptates dolor eveniet. Omnis voluptas et ut est porro soluta ut est. Voluptatem dolore vero in. A aut iste et unde autem ut deserunt quam. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia. Nam perferendis facilis asperiores ea qui voluptates dolor eveniet. Omnis voluptas et ut est porro soluta ut est. Voluptatem dolore vero in. A aut iste et unde autem ut deserunt quam. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia. Nam perferendis facilis asperiores ea qui voluptates dolor eveniet. Omnis voluptas et ut est porro soluta ut est. Voluptatem dolore vero in. A aut iste et unde autem ut deserunt quam. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat.</p>
+		</div>
+	</div>
+</section>
 `
 
+
+
+// <h1 class="text-5xl font-bold text-center">Transcendence</h1>
+// <div id="img-home">
+// 	<img src="/images/pong.avif" alt="ping" class="flex-shrink-0 p-10">
+// </div>
+// <div id="btn-home" class="grid grid-cols-[1fr_0.1fr_1fr] grid-rows-2 p-4 gap-5 size-fit mx-auto">
+// 	<button id="btn-signup" class="generic_btn col-start-1 col-end-2 p-2">Sign up</button>
+// 	<button id="btn-signin" class="generic_btn col-start-3 col-end-4 p-2">Sign in</button>
+// 	<button id="btn-google" class="generic_btn col-span-3 p-2 px-4 flex justify-center">
+// 		<img src="/images/l-ggl.webp" alt="Google_logo" class="size-7 inline -ml-3">
+// 		Continue with Google
+// 	</button>
+// </div>
 
 
 // <div class="min-h-[80vh]">
