ab817bde4ab4c542a590c29539f103f83adc1d5a
Acancel <acancel@student.42lyon.fr>
2025-12-04 22:13:05 +0100

    feat(service/fontend/app): add new font for main title

--- services/frontend/app/tailwind.config.js
+++ services/frontend/app/tailwind.config.js
@@ -1,17 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./index.html', './src/**/*.{html,js,ts}'],
-	theme: { extend: {} },
-	plugins: [],
-};
-
-
-module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        amado: ['Amado', 'sans-serif'],
-      },
-    },
-  },
-};
+	export default {
+			content: ['./index.html', './src/**/*.{html,js,ts}'],
+			theme: {
+					extend: {
+							fontFamily: {
+									HoldMoney: ['HoldMoney', 'sans-serif'],
+							},
+					},
+			},
+			plugins: [],
+	};
