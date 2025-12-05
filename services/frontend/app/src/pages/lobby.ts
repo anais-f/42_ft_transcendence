118061b5beab24a24b06fa250d85b03e3014db93
Acancel <acancel@student.42lyon.fr>
2025-12-05 20:28:36 +0100

    feat(service/frontend/app/src): update Lobby_page content

--- services/frontend/app/src/pages/lobby.ts
+++ services/frontend/app/src/pages/lobby.ts
@@ -1,37 +1,74 @@
 export const LobbyPage = (): string => /*html*/ `
-<div class="flex flex-col items-center justify-start h-[85vh] w-full p-4">
-      <h1>Lobby number : ${lobby_number}</h1>
-      <h2 class="mt-4 mb-20">Share this code to invite other player to join...</h2>
-      <div class="grid grid-cols-[1fr_0.5fr_1fr] grid-rows-3 gap-4 mt-4 w-full h-[40%]">
-        <span class="col-span-3 text-center">Players:</span>
-        <div class="col-start-1 col-end-2 row-start-2 row-span-2 bg-gray-200 p-4 rounded-md border border-gray-400 shadow-xl">
-          <h2 class="text-center">Player 1</h2>
-          <div class="flex flex-col items-center mt-4">
-            <img src=${p1.avatar} alt="Player 1 Avatar" class="rounded-full w-1/3 aspect-square object-cover flex-shrink-0 border-4 border-gray-400 shadow-xl">
-            <h2 class="text-center my-2">${p1.username}</h2>
-          </div>
-        </div>
-        <div class="col-start-3 row-start-2 row-span-2 bg-gray-200 p-4 rounded-md border border-gray-400 shadow-xl">
-          <h2 class="text-center">Player 2</h2>
-                <div class="flex flex-col items-center mt-4">
-            <img src=${p2.avatar} alt="Player 1 Avatar" class="rounded-full w-1/3 aspect-square object-cover flex-shrink-0 border-4 border-gray-400 shadow-xl">
-            <h2 class="text-center my-2">${p2.username}</h2>
-          </div>
-        </div>
+<section class="grid grid-cols-4 gap-11">
+	<div class="col-span-1 flex flex-col items-start">
+    <div class="flex flex-row justify-between pt-10 pb-5 w-full">
+		  <h1 class="text-2xl select-none ">LOBBY NUMBER :</h1> 
+      <span class="text-2xl ">${lobby_number}</span>
+    </div>
+    <button id="btn-copy" class="generic_btn mb-4">
+      Copy Lobby Code
+    </button>
+    <div class="text-xl py-4 text-justify select-none">
+      Dear reader,<br>
+      We cordially invite you to share the code above with a friend so that you can invite them to play.
+    </div>
+		<div class="news_paragraph">
+			<h1 class="text-lg pt-4">Title</h1>
+			<p class="text-sm py-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia. Assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia assumenda reprehenderit nesciunt. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia assumenda reprehenderit nesciunt. Eos debitis officia. Assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia assumenda reprehenderit nesciunt. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia assumenda reprehenderit nesciunt.</p>
+		</div>
+	</div>
 
-        </div>
-      </div>
-</div>
-  `
+	<div class="col-span-1 flex flex-col items-start">
+		<div class="news_paragraph">
+			<p class="text-sm pt-6 pb-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia. Assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat.</p>
+		</div>
+		<h1 class="text-2xl pt-4 pb-1">PLAYER 1</h1>
+		<img src="${p1.avatar}" alt="Player_1" class="w-[90%] object-cover aspect-square object-center select-none">
+    <span class="text-xl pt-2 pb-4">${p1.username}</span>
+		<div class="news_paragraph">
+			<h1 class="text-lg pt-4">Title</h1>
+			<p class="text-sm py-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Voluptates dolores doloremque. Beatae qui et placeat. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. </p>
+		</div>
+	</div>
+
+	<div class="col-span-1 flex flex-col items-start">
+		<div class="news_paragraph">
+			<p class="text-sm pt-6 pb-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia.</p>
+		</div>
+		<img src="/assets/images/swords.png" alt="Player_1" class="w-[100%] object-cover aspect-square object-center select-none opacity-30 py-8">
+     <button id="btn-copy" class="generic_btn mt-4">
+      Ready
+    </button>
+		<div class="news_paragraph">
+			<h1 class="text-lg pt-8">Title</h1>
+			<p class="text-sm py-2">Eos debitis officia assumenda reprehenderit nesciunt. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia assumenda reprehenderit nesciunt. Eos debitis officia. Assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem.</p>
+		</div>
+	</div>
+
+	</div>
+		<div class="col-span-1 flex flex-col items-start">
+		<div class="news_paragraph">
+			<p class="text-sm pt-6 pb-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia. Assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat.</p>
+		</div>
+		<h1 class="text-2xl pt-4 pb-1">PLAYER 2</h1>
+		<img src="${p2.avatar}" alt="Player_2" class="w-[90%] object-cover aspect-square object-center select-none">
+    <span class="text-xl pt-2 pb-6">${p2.username}</span>
+		<div class="news_paragraph">
+			<p class="text-sm py-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus.</p>
+		</div>
+	</div>
+	</div>
+</section>
+`
 
 let lobby_number = "XXXX-XXXX";
 
 let p1 = {
   username: "Anfichet",
-  avatar: "/images/anfichet.jpeg"
+  avatar: "assets/images/anfichet.jpeg"
 };
 
 let p2 = {
   username: "Lrio",
-  avatar: "/images/lrio.jpg"
+  avatar: "assets/images/lrio.jpg"
 };
