e5083968c34457b76311964c5307c1f4648ff48a
Acancel <acancel@student.42lyon.fr>
2025-12-05 20:30:08 +0100

    feat(service/frontend/app/src): update Settings_page content

--- services/frontend/app/src/pages/settings.ts
+++ services/frontend/app/src/pages/settings.ts
@@ -1,10 +1,10 @@
-export const SettingPage = (): string => /*html*/ `
+export const SettingsPage = (): string => /*html*/ `
 <section class="grid grid-cols-4 gap-11">
 	<div class="col-span-1 flex flex-col items-start">
 		<h1 class="text-2xl py-4">WOULD LIKE TO ...</h1>
 		<div class="news_paragraph">
 			<h1 class="text-lg pt-4">Title</h1>
-			<p class="text-sm py-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia. Assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui.  Assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi.</p>
+			<p class="text-sm py-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia. Assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem.</p>
 		</div>
 		<h1 class="text-2xl py-4">CHANGE USERNAME ?</h1>
 		<form id="change_username_form" class="flex flex-col gap-2 w-full">
@@ -13,8 +13,23 @@ export const SettingPage = (): string => /*html*/ `
 		</form>
 		<div class="news_paragraph">
 			<h1 class="text-lg pt-4">Title</h1>
-			<p class="text-sm py-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia. Assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi.</p>
+			<p class="text-sm py-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia. Assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi.  Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi.  Consectetur minus maiores qui.  Assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae.</p>
 		</div>
+	</div>
+	
+	<div class="col-span-1 flex flex-col items-start">
+		<div class="news_paragraph">
+			<p class="text-sm py-6">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem.</p>
+		</div>
+		<img src="/assets/images/paddle.png" alt="paddle" class="w-full object-cover opacity-50 select-none">
+		<h1 class="text-2xl pt-8 pb-1">CHANGE PASSWORD ?</h1>
+		<form id="change_password_form" class="flex flex-col gap-2 w-full">
+			<input id="change_password" type="password" name="change_password" class=" px-2 border-b-2 text-xl border-black bg-inherit w-full font-[Birthstone]" placeholder="NEW PASSWORD" required>
+			<input id="change_password_confirm" type="password" name="change_password_confirm" class=" px-2 border-b-2 text-xl border-black bg-inherit w-full font-[Birthstone]" placeholder="CONFIRM NEW PASSWORD" required>
+			<input id="change_password_2fa" type="text" name="change_password_2fa" class=" px-2 border-b-2 text-xl border-black bg-inherit w-full font-[Birthstone]" placeholder="2FA CODE" required>
+			<button id="change_password_btn" class="generic_btn mt-4" type="submit">Save it</button>
+		</form>
+		
 	</div>
 
 	<div class="col-span-1 flex flex-col items-start">
@@ -33,24 +48,6 @@ export const SettingPage = (): string => /*html*/ `
 	</div>
 
 	<div class="col-span-1 flex flex-col items-start">
-		<div class="news_paragraph">
-			<p class="text-sm py-6">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem.</p>
-		</div>
-		<img src="/assets/images/image_9.png" alt="AHHHHH !" class="w-full object-cover opacity-50 select-none">
-		<h1 class="text-2xl pt-8 pb-1">CHANGE PASSWORD ?</h1>
-		<form id="change_password_form" class="flex flex-col gap-2 w-full">
-			<input id="change_password" type="password" name="change_password" class=" px-2 border-b-2 text-xl border-black bg-inherit w-full font-[Birthstone]" placeholder="NEW PASSWORD" required>
-			<input id="change_password_confirm" type="password" name="change_password_confirm" class=" px-2 border-b-2 text-xl border-black bg-inherit w-full font-[Birthstone]" placeholder="CONFIRM NEW PASSWORD" required>
-			<input id="change_password_2fa" type="text" name="change_password_2fa" class=" px-2 border-b-2 text-xl border-black bg-inherit w-full font-[Birthstone]" placeholder="2FA CODE" required>
-			<button id="change_password_btn" class="generic_btn mt-4" type="submit">Save it</button>
-		</form>
-		<div class="news_paragraph">
-			<h1 class="text-lg pt-6">Title</h1>
-			<p class="text-sm pb-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia.</p>
-		</div>
-		
-	</div>
-	<div class="col-span-1 flex flex-col items-start">
 		<div id="disable_2fa" class="w-full">
 			<h1 class="text-2xl pt-8 pb-1">DISABLE 2FA ?</h1>
 			<form id="disable_2fa_form" class="flex flex-col gap-2 w-full">
@@ -59,13 +56,10 @@ export const SettingPage = (): string => /*html*/ `
 				<button id="disable_2fa_btn" class="generic_btn mt-4" type="submit">Disable 2FA</button>
 			</form>
 		</div>
+		<img src="/assets/images/tiger.png" alt="tiger" class="w-full object-cover opacity-50 select-none">
 		<div class="news_paragraph">
 			<h1 class="text-lg pt-6">Title</h1>
 			<p class="text-sm py-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia. Nam perferendis facilis asperiores ea qui voluptates dolor eveniet. Omnis voluptas et ut est porro soluta ut est.</p>
-		</div>
-		<img src="/assets/images/image_10.png" alt="AHHHHH !" class="w-full object-cover opacity-50 select-none">
-		<div class="news_paragraph">
-			<p class="text-sm py-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia. Nam perferendis facilis asperiores ea qui voluptates dolor eveniet.</p>
 		</div>
 	</div>
 </section>
