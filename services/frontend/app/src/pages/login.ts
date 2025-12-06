7d692e9218881bfdac99a68d8209f987c2e85c42
Acancel <acancel@student.42lyon.fr>
2025-12-06 18:44:40 +0100

    feat(service/frontend/app): add login logic

--- services/frontend/app/src/pages/login.ts
+++ services/frontend/app/src/pages/login.ts
@@ -1,13 +1,16 @@
+export const LoginPage = (): string => {
 
-export const LoginPage = (): string => /*html*/ `
+	return /*html*/ `
 <section class="grid grid-cols-4 gap-11">
 	<div class="col-span-1 flex flex-col items-start">
 		<h1 class="text-2xl py-4">SUBSCRIBE TO OUR NEWSPAPER</h1>
-		<form id="register_form" class="flex flex-col gap-2">
-		<input id="register_username" type="text" name="register_username" class=" px-2 border-b-2 text-xl border-black bg-inherit w-full font-[Birthstone]" placeholder="USERNAME" required>
-		<input id="register_password" type="password" name="register_password" class=" px-2 border-b-2 text-xl border-black bg-inherit w-full font-[Birthstone]" placeholder="PASSWORD" required>
-		<input id="register_conf_password" type="password" name="register_conf_password" class=" px-2 border-b-2 text-xl border-black bg-inherit w-full font-[Birthstone]" placeholder="CONFIRM PASSWORD" required>
-		<button id="register_btn" class="generic_btn mt-4" type="submit">Register</button>
+		<form id="register_form" onsubmit="(e) => {console.log('Submit', e)
+		e.preventDefault()}" class="flex flex-col gap-2">
+			<input id="register_username" type="text" name="register_username" class=" px-2 border-b-2 text-xl border-black bg-inherit w-full font-[Birthstone]" placeholder="USERNAME" required>
+			<input id="register_password" type="password" name="register_password" class=" px-2 border-b-2 text-xl border-black bg-inherit w-full font-[Birthstone]" placeholder="PASSWORD" required>
+			<input id="register_conf_password" type="password" name="register_conf_password" class=" px-2 border-b-2 text-xl border-black bg-inherit w-full font-[Birthstone]" placeholder="CONFIRM PASSWORD" required>
+			<button id="register_btn" class="generic_btn mt-4" type="submit">Register</button>
+		</form>
 		<div class="news_paragraph">
 			<h1 class="text-lg pt-4">Title</h1>
 			<p class="text-sm py-2">Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia. Assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia assumenda reprehenderit nesciunt. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia assumenda reprehenderit nesciunt. Eos debitis officia. Assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Eaque optio non quae. Vel sunt in et rem. Quidem qui autem assumenda reprehenderit nesciunt. Voluptates dolores doloremque. Beatae qui et placeat. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia assumenda reprehenderit nesciunt. Ipsum dolore veritatis odio in ipsa corrupti aliquam qui commodi. Eveniet possimus voluptas voluptatem. Consectetur minus maiores qui. Eos debitis officia assumenda reprehenderit nesciunt.</p>
@@ -57,37 +60,52 @@ export const LoginPage = (): string => /*html*/ `
 	</div>
 </section>
 `
+}
 
+export function bindRegisterForm() {
+	console.log('Bind called')
+	const formReg = document.getElementById('register_form')
+	if (!formReg)
+		return console.log('Error no form found')
+	console.log('founded')
+	formReg.addEventListener('submit', async (e) => {
+		e.preventDefault()
+		console.log(e)
+		console.log(e.target)
+		if (!e.target)
+			return
+		const formData = new FormData(formReg as HTMLFormElement)
+		console.log(formData)
+		console.log(formData.get('register_password'))
+		const pw = formData.get('register_password')
+		const us = formData.get('register_username')
+		if (!pw)
+			return
+		if (pw != formData.get('register_conf_password'))
+			return console.log("passwords don't match")
+		const pwS = pw.toString()
+		if (pwS.length < 8)
+			return (console.log('pw too short'))
+		const user = {
+			login: us,
+			password: pw
+		}
+		try {
 
+			const res = await fetch('/auth/api/register', {
+				method: 'POST',
+				headers: {
+					'content-type': 'application/json'
+				},
+				body: JSON.stringify(user)
+			})
+			if (!res.ok)
+				console.error(res.status)
+			window.navigate('/')
+		}
+		catch (e) {
+			console.log(e)
+		}
 
-// <h1 class="text-5xl font-bold text-center">Transcendence</h1>
-// <div id="img-home">
-// 	<img src="/images/pong.avif" alt="ping" class="flex-shrink-0 p-10">
-// </div>
-// <div id="btn-home" class="grid grid-cols-[1fr_0.1fr_1fr] grid-rows-2 p-4 gap-5 size-fit mx-auto">
-// 	<button id="btn-signup" class="generic_btn col-start-1 col-end-2 p-2">Sign up</button>
-// 	<button id="btn-signin" class="generic_btn col-start-3 col-end-4 p-2">Sign in</button>
-// 	<button id="btn-google" class="generic_btn col-span-3 p-2 px-4 flex justify-center">
-// 		<img src="/images/l-ggl.webp" alt="Google_logo" class="size-7 inline -ml-3">
-// 		Continue with Google
-// 	</button>
-// </div>
-
-
-// <div class="min-h-[80vh]">
-//     <h1 class="text-8xl text-center text-orange-400 bg-[url(/images/pong.avif)] bg-no-repeat bg-center py-48">
-//       ft_transcendence</h1>
-//     <div class="flex place-content-center">
-//       <a href="#register">
-//         <button class="bg-gray-300 m-5 p-3 rounded-md border border-gray-400  hover:bg-gray-400 shadow-xl">Sign in</button>
-//       </a>
-//       <a href="#wales">
-//         <button class="bg-gray-300 m-5 p-3 rounded-md border border-gray-400  hover:bg-gray-400 shadow-xl">Sign up</button>
-//       </a>
-//         <a href="#me" class="flex justify-between">
-//           <button class="bg-gray-100 place-center m-5 rounded-lg p-3 px-4 hover:bg-gray-400 shadow-xl">
-//             <img src="/images/Google_logo_svg.webp" alt="Google_logo" class="size-7 inline -ml-3">
-//             Continue with Google</button>
-//         </a>
-//     </div>
-// </div>
+	})
+} 
