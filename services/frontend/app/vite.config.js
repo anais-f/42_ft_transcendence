77c23090c58b4d1cc8b2949d3f77abf301a9fea9
Acancel <acancel@student.42lyon.fr>
2025-12-06 18:41:09 +0100

    chore(service/frontend/app): up vite-config

--- services/frontend/app/vite.config.js
+++ services/frontend/app/vite.config.js
@@ -4,4 +4,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
 	base: './',
 	build: { outDir: 'dist', emptyOutDir: true },
+	server: {
+		host: true,
+		port: 3000,
+		historyApiFallback: true
+	}
 })
