51c0dc5f268a1ddffb770efd797608cfc730f222
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-12-07 21:03:19 +0100

    feat: add pendingGame map (#110)

--- services/pong-server/app/game/gameManager/gamesData.ts
+++ services/pong-server/app/game/gameManager/gamesData.ts
@@ -1,15 +1,19 @@
 import { IGameData } from '../../utils/createGame'
 
-interface Iplayer {
+export type GameStatus = 'pending' | 'active' | 'finished'
+
+export interface Iplayer {
 	id: number
 	connState: boolean
 }
-export const games = new Map<
-	string,
-	{
-		p1: Iplayer | undefined
-		p2: Iplayer | undefined
-		gameInstance: IGameData | undefined
-	}
->()
-export const playerToGame = new Map<number, string>()
+
+export interface GameData {
+	p1: Iplayer | undefined
+	p2: Iplayer | undefined
+	gameInstance: IGameData | undefined
+	status: GameStatus
+}
+
+export const games = new Map<string, GameData>()
+export const playerToActiveGame = new Map<number, string>()
+export const playerToPendingGame = new Map<number, string>()
