44dde4cd8141624f66fe4e5be924dc96974f2869
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-12-07 21:17:04 +0100

    fix: leaveGame (#110)

--- services/pong-server/app/game/gameManager/leaveGame.ts
+++ services/pong-server/app/game/gameManager/leaveGame.ts
@@ -1,13 +1,14 @@
-import { playerToGame, games } from './gamesData.js'
+import { games, playerToActiveGame } from './gamesData.js'
 
 /*
- * Function to remove a player from their current game
+ * Function to remove a player from their current active game
  * If both players have left, the game is deleted
+ * Sets the game status to 'finished' when a player leaves
  * @param playerId - The ID of the player leaving
  * @return nothing
  */
 export function leaveGame(playerId: number) {
-	const gameCode = playerToGame.get(playerId)
+	const gameCode = playerToActiveGame.get(playerId)
 	if (!gameCode) return
 
 	const gameData = games.get(gameCode)
@@ -15,9 +16,11 @@ export function leaveGame(playerId: number) {
 		if (gameData.p1?.id === playerId) gameData.p1 = undefined
 		if (gameData.p2?.id === playerId) gameData.p2 = undefined
 
+		gameData.status = 'finished'
+
 		if (!gameData.p1 && !gameData.p2) {
 			games.delete(gameCode)
 		}
 	}
-	playerToGame.delete(playerId)
+	playerToActiveGame.delete(playerId)
 }
