81ae678328d0a32e3e4ef391ea2406a4a9ff8be7
Michel Juffard <juffard.michel@gmail.com>
2025-12-09 13:48:17 +0100

    feat: add schema for saving match.

--- packages/common/srcs/DTO/matchmakingSchema.ts
+++ packages/common/srcs/DTO/matchmakingSchema.ts
@@ -16,5 +16,16 @@ export const RemoveTournamentSchema = z
 	})
 	.strict()
 
+export const saveMatchShema = z
+	.object({
+		player1Id: number().positive(),
+		player2Id: number().positive(),
+		scorePlayer1: number().nonnegative(),
+		scorePlayer2: number().nonnegative(),
+		idTournament: number(), //-1 if no tournament
+		round: number() //-1 no tournament
+	})
+	.strict()
+
 export type CreateTournamentDTO = z.infer<typeof CreateTournamentSchema>
 export type RemoveTournamentDTO = z.infer<typeof RemoveTournamentSchema>
