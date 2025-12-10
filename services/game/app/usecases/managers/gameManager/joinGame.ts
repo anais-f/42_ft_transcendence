19f3d88316f25236661de74608e4b6931867f74b
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-12-10 19:53:33 +0100

    fix(gameManager): join game
    use connstate to know if player already have an active game

--- services/game/app/usecases/managers/gameManager/joinGame.ts
+++ services/game/app/usecases/managers/gameManager/joinGame.ts
@@ -3,25 +3,25 @@ import { startTimeOut } from './startTimeOut.js'
 
 export function joinGame(gameCode: string, pID: number) {
 	const gameData = games.get(gameCode)
-	if (undefined == gameData) {
-		throw new Error('unknow game code')
+	if (!gameData) {
+		throw new Error('unknown game code')
 	}
 
-	// TODO: test connstate instead of playerToGame
-	//	if (playerToGame.has(pID)) {
-	//		throw new Error('player is already in a game')
-	//	}
-
-	if (gameData.p1 && gameData.p1.id == pID) {
+	if (gameData.p1.id === pID) {
+		if (gameData.p1.connState) {
+			throw new Error('player is already in a game')
+		}
 		return
 	}
 
-	if (gameData.p2 && gameData.p2.id == pID) {
+	if (gameData.p2?.id === pID) {
+		if (gameData.p2.connState) {
+			throw new Error('player is already in a game')
+		}
 		return
 	}
 
-	if (undefined == gameData.p2) {
-		// open game
+	if (!gameData.p2) {
 		gameData.p2 = { id: pID, connState: false }
 		startTimeOut(pID, 5000)
 		return
