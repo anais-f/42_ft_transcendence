1bc8e9cce7faa1d91dcb7f93dd189a7fee6bcf19
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-12-12 18:32:47 +0100

    feat(frontend): add playerSlot traking and redirection

--- services/frontend/app/src/usecases/gameStore.ts
+++ services/frontend/app/src/usecases/gameStore.ts
@@ -1,7 +1,11 @@
+export type PlayerSlot = 'p1' | 'p2'
+
 class GameStore {
 	private gameCode: string | null = null
 	private sessionToken: string | null = null
 	private gameSocket: WebSocket | null = null
+	private _navigatingToGame: boolean = false
+	private _playerSlot: PlayerSlot = 'p1'
 
 	getGameCode(): string | null {
 		return this.gameCode
@@ -27,6 +31,22 @@ class GameStore {
 		this.gameSocket = socket
 	}
 
+	get navigatingToGame(): boolean {
+		return this._navigatingToGame
+	}
+
+	set navigatingToGame(value: boolean) {
+		this._navigatingToGame = value
+	}
+
+	get playerSlot(): PlayerSlot {
+		return this._playerSlot
+	}
+
+	set playerSlot(slot: PlayerSlot) {
+		this._playerSlot = slot
+	}
+
 	clear(): void {
 		if (this.gameSocket) {
 			this.gameSocket.close()
@@ -34,6 +54,8 @@ class GameStore {
 		this.gameCode = null
 		this.sessionToken = null
 		this.gameSocket = null
+		this._navigatingToGame = false
+		this._playerSlot = 'p1'
 	}
 }
 
