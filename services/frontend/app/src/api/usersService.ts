23bfd8935c72993d84bed94240fb7aa5c6ccc058
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-12-12 18:31:46 +0100

    feat(frontend): add users API service utils

--- services/frontend/app/src/api/usersService.ts
+++ services/frontend/app/src/api/usersService.ts
@@ -1,14 +1,17 @@
 import { IPublicProfileUser } from '@ft_transcendence/common'
 
-export async function fetchUserById(userId: number): Promise<IPublicProfileUser | null> {
+export async function fetchUserById(
+	userId: number
+): Promise<IPublicProfileUser | null> {
 	try {
 		const response = await fetch(`/users/api/users/profile/${userId}`, {
 			method: 'GET',
 			credentials: 'include'
 		})
 
-		if (!response.ok) return null
-
+		if (!response.ok) {
+			return null
+		}
 		return (await response.json()) as IPublicProfileUser
 	} catch (error) {
 		console.error('Failed to fetch user:', error)
