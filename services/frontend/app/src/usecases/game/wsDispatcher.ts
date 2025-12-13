9cc2574c9e5d7f0b04277e16dac1657c31f6026a
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-12-13 01:43:11 +0100

    feat(frontend): add S07 && S08 in packet dispatcher

--- services/frontend/app/src/usecases/game/wsDispatcher.ts
+++ services/frontend/app/src/usecases/game/wsDispatcher.ts
@@ -1,11 +1,15 @@
 import { packetBuilder } from '@ft_transcendence/pong-shared/network/Packet/packetBuilder.js'
 import { S02SegmentUpdate } from '@ft_transcendence/pong-shared/network/Packet/Server/S02.js'
 import { S06BallSync } from '@ft_transcendence/pong-shared/network/Packet/Server/S03/S06.js'
+import { S07Score } from '@ft_transcendence/pong-shared/network/Packet/Server/S07.js'
+import { S08Countdown } from '@ft_transcendence/pong-shared/network/Packet/Server/S08.js'
 import { eogHandler } from './handlers/eogHandler.js'
 import { opponentHandler } from './handlers/opponentHandler.js'
 import { startingInHandler } from './handlers/startingInHandler.js'
 import { startHandler } from './handlers/startHandler.js'
 import { slotHandler } from './handlers/slotHandler.js'
+import { scoreHandler } from './handlers/scoreHandler.js'
+import { countdownHandler } from './handlers/countdownHandler.js'
 import { gameRenderer } from './gameRenderer.js'
 
 type JsonMessage = {
@@ -53,5 +57,9 @@ function handleBinaryMessage(data: ArrayBuffer) {
 			packet.getVelo(),
 			packet.getFactor()
 		)
+	} else if (packet instanceof S07Score) {
+		scoreHandler(packet)
+	} else if (packet instanceof S08Countdown) {
+		countdownHandler(packet)
 	}
 }
