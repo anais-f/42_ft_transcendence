fc75c91f2e390ff4608a3e970f9dac0889b9658d
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-12-15 22:59:46 +0100

    feat: allow all hostname

--- services/frontend/app/vite.config.js
+++ services/frontend/app/vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
 	base: './',
 	build: { outDir: 'dist', emptyOutDir: true },
 	server: {
-		host: true,
+		host: '0.0.0.0',
+		allowedHosts: true,
 		port: 3000,
 		historyApiFallback: true
 	}
