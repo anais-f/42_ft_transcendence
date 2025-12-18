a897953d5bbdd92142089f1eedaf1295301e36d9
Michel Juffard <juffard.michel@gmail.com>
2025-12-18 18:31:14 +0100

    fix: recreate historyController

---
+++ services/game/app/controllers/historyControllers.ts
@@ -0,0 +1,22 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import {
+	IdParamSchema,
+	type MatchHistoryResponseDTO
+} from '@ft_transcendence/common'
+import { getUserMatchHistory } from '../usecases/historyUsecases.js'
+import createHttpError from 'http-errors'
+
+export function getUserMatchHistoryController(
+	request: FastifyRequest,
+	reply: FastifyReply
+) {
+	const userId = request.user.user_id
+	if (userId === undefined) {
+		throw createHttpError.Unauthorized()
+	}
+	const targetUserId = IdParamSchema.parse(request.params).id
+	const result: MatchHistoryResponseDTO = getUserMatchHistory(
+		Number(targetUserId)
+	)
+	return reply.send(result)
+}
