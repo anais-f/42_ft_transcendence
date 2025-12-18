015c405afe40dea1fe8f1fed32314b72c3cf0a10
Michel Juffard <juffard.michel@gmail.com>
2025-12-18 17:49:48 +0100

    feat(stats): add user stats and match history routes and controllers

---
+++ services/game/app/routes/statsRoutes.ts
@@ -0,0 +1,40 @@
+import { FastifyInstance } from 'fastify'
+import { jwtAuthMiddleware } from '@ft_transcendence/security'
+import {
+	getUserMatchHistoryController,
+	getUserStatsController
+} from '../controllers/statsControllers.js'
+import {
+	IdParamSchema,
+	MatchHistoryResponseSchema,
+	PlayerStatsSchema
+} from '@ft_transcendence/common'
+
+export function statsRoutes(app: FastifyInstance) {
+	app.get(
+		'/api/user/matchHistory/:id',
+		{
+			schema: {
+				params: IdParamSchema,
+				response: {
+					200: MatchHistoryResponseSchema
+				}
+			},
+			preHandler: jwtAuthMiddleware
+		},
+		getUserMatchHistoryController
+	)
+	app.get(
+		'/api/user/stats/:id',
+		{
+			schema: {
+				params: IdParamSchema,
+				response: {
+					200: PlayerStatsSchema
+				}
+			},
+			preHandler: jwtAuthMiddleware
+		},
+		getUserStatsController
+	)
+}
