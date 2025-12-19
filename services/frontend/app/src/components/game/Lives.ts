b840b57609cd5681151bec948585da7bbd114281
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-12-19 02:14:11 +0100

    feat: update life shape

--- services/frontend/app/src/components/game/Lives.ts
+++ services/frontend/app/src/components/game/Lives.ts
@@ -23,11 +23,13 @@ export const LivesComp = (props: LivesProps): string => {
 function generateHearts(max: number, current: number): string {
 	return Array.from({ length: max }, (_, index) => {
 		const isFilled = index < current
-		const heartClass = isFilled
-			? 'w-5 h-5 bg-gray-950 rounded-xl'
-			: 'w-5 h-5 bg-gray-950 rounded-xl opacity-35'
+		const fillColor = isFilled ? '#000000ff' : '#00000066'
 
-		return `<div class="${heartClass}"></div>`
+		return `
+		<svg class="w-5, h-5" viewBox="0 0 16 16" fill="${fillColor}">
+			<path d="M11.75,1C10.126,1,8.716,1.911,8,3.249C7.284,1.911,5.874,1,4.25,1C1.903,1,0,2.903,0,5.25C0,11,8,15,8,15s8-4,8-9.75 C16,2.903,14.097,1,11.75,1z" />
+		</svg>
+        `
 	}).join('')
 }
 
