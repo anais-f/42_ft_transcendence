9d2cab088262604ee914ddd400797fe889a5650f
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2025-12-24 14:22:03 +0100

    feat: status circle comp

--- services/frontend/app/src/components/friends/FriendListItem.ts
+++ services/frontend/app/src/components/friends/FriendListItem.ts
@@ -1,4 +1,5 @@
 import { UserLink } from '../UserLink.js'
+import { StatusCircle } from './StatusCircle.js'
 
 /**
  * Renders a friend list item.
@@ -25,21 +26,22 @@ export const FriendListItem = (props: FriendListItemProps): string => {
 	const { id, username, avatar, status } = props
 
 	const isOnline = status === 1
-	const statusColor = isOnline ? 'bg-green-500' : 'bg-gray-500'
 	const statusText = isOnline ? 'Online' : 'Offline'
 
 	return /*html*/ `
     <li class="flex flex-row border-b border-black" id="friend_item_${id}">
       <div class="flex gap-4 py-2 px-4">
+        <!-- User's avatar -->
         <img src="${avatar}" alt="${username}'s avatar" class="w-12 h-12 object-cover border-black">
         <div>
+          <!-- User's link -->
           ${UserLink({ id, username })}
           <p class="text-gray-500 flex items-center gap-2">
-            <span id="status_circle_${id}" class="w-3 h-3 rounded-full ${statusColor}"></span>
+            ${StatusCircle({ isOnline })}
             <span id="status_text_${id}">${statusText}</span>
           </p>
         </div>
       </div>
     </li>
-    `
+  `
 }
