d6a24e8f68019ab47ee5a2258a4b8b54239e12b5
Michel Juffard <juffard.michel@gmail.com>
2025-12-30 08:08:46 +0100

    chore: remove  for internal routes and use it for nginx

--- services/game/app/index.ts
+++ services/game/app/index.ts
@@ -43,10 +43,10 @@ async function start(): Promise<void> {
 		await app.register(metricPlugin.default, { endpoint: '/metrics' })
 		await app.ready()
 		await app.listen({
-			port: env.PORT,
+			port: 3000,
 			host: '0.0.0.0'
 		})
-		console.log(`listening on port: ${env.PORT}`)
+		console.log(`listening on port: 3000`)
 	} catch (err) {
 		console.error('Error starting server:', err)
 		process.exit(1)
--- services/nginx/entrypoint.sh
+++ services/nginx/entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-exec nginx -g 'daemon off;'
+envsubst '$HOSTNAME $PORT' < /etc/nginx/http.d/default.conf.template > /etc/nginx/http.d/default.conf
+cat /etc/nginx/http.d/default.conf
+exec nginx -g 'daemon off;' 
