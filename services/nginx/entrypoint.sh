e75c009310f1fe946985d8f57a34de9491c8987b
Michel Juffard <juffard.michel@gmail.com>
2026-01-06 16:23:44 +0100

    chore: remove print of conf and add a message when nginx start to launch

--- services/nginx/entrypoint.sh
+++ services/nginx/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 envsubst '$HOSTNAME $PORT $USERS_SERVICE_URL $TWOFA_SERVICE_URL $AUTH_SERVICE_URL $SOCIAL_SERVICE_URL $GAME_SERVICE_URL $SUBNET_DOCKER_MONITORING' < /etc/nginx/http.d/default.conf.template > /etc/nginx/http.d/default.conf
-cat /etc/nginx/http.d/default.conf
-exec nginx -g 'daemon off;' 
+echo "Nginx launched"
+exec nginx -g 'daemon off;'
