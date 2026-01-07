0a0f15e7ccdb457b987d774ccc61e82e70fc9ccd
Michel Juffard <juffard.michel@gmail.com>
2026-01-07 19:00:35 +0100

    chore: add response shema

--- services/game/app/routes/cleanupRoutes.ts
+++ services/game/app/routes/cleanupRoutes.ts
@@ -2,13 +2,31 @@ import { FastifyInstance } from 'fastify'
 import { apiKeyMiddleware } from '@ft_transcendence/security'
 import { cleanupUserController } from '../controllers/cleanupController.js'
 import { UserIdCoerceSchema } from '@ft_transcendence/common'
+import { z } from 'zod'
+import { HttpErrorSchema } from '@ft_transcendence/common'
 
 export function cleanupRoutes(app: FastifyInstance) {
 	app.post(
 		'/api/game/internal/cleanup/:user_id',
 		{
 			schema: {
+				description:
+					'Cleans up all game-related data for the specified user. Used when a user is deleted.',
 				tags: ['game', 'tournament'],
+				response: {
+					200: z
+						.any()
+						.meta({ description: 'User cleanup completed successfully' }),
+					400: HttpErrorSchema.meta({
+						description: 'User_id is required'
+					}),
+					404: HttpErrorSchema.meta({
+						description: 'User is not in any tournament/Tournament not found/'
+					}),
+					409: HttpErrorSchema.meta({
+						description: "You can't quit a tournament that has already started"
+					})
+				},
 				params: UserIdCoerceSchema
 			},
 			preHandler: apiKeyMiddleware
